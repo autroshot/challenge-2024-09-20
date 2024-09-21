5d96d80f6eaca0dcac212171d5419e7aabbb5c98
autroshot <autroshot@gmail.com>
2024-09-21 16:26:53 +0900

    전체 요소에 letter spacing 스타일 적용

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,5 +1,5 @@
 export default function Title({ children }: Props) {
-    return <h1 className="font-exo-2 text-5xl leading-[4.5rem]">{children}</h1>;
+    return <h1 className="font-exo-2 text-5xl leading-[4.5rem] tracking-[-0.045rem]">{children}</h1>;
 }
 
 interface Props {
--- src/components/profileSection/Card.tsx
+++ src/components/profileSection/Card.tsx
@@ -2,9 +2,11 @@ export default function Card({ imageSrc, title, description }: Props) {
     return (
         <div>
             <img src={imageSrc} alt="아바타" className="w-[6.75rem]" />
-            <h2 className="mt-10 font-montserrat text-2xl font-bold leading-9">{title}</h2>
-            <p className="mt-6 font-montserrat text-lg leading-[1.875rem] text-[#000000cc]">{description}</p>
-            <button className="mt-6 font-exo-2 text-lg font-bold leading-[1.875rem] text-[#18a0fb] hover:underline">
+            <h2 className="mt-10 font-montserrat text-2xl font-bold leading-9 tracking-[-0.023rem]">{title}</h2>
+            <p className="mt-6 font-montserrat text-lg leading-[1.875rem] tracking-[-0.017rem] text-[#000000cc]">
+                {description}
+            </p>
+            <button className="mt-6 font-exo-2 text-lg font-bold leading-[1.875rem] tracking-[-0.017rem] text-[#18a0fb] hover:underline">
                 LEARN MORE
             </button>
         </div>
