bdfeae2969cfa134708d73dbe5a917db07341492
autroshot <autroshot@gmail.com>
2024-09-21 16:14:27 +0900

    리팩터: 프리티어 포맷팅 적용

--- src/components/profileSection/Card.tsx
+++ src/components/profileSection/Card.tsx
@@ -2,9 +2,9 @@ export default function Card({ imageSrc, title, description }: Props) {
     return (
         <div>
             <img src={imageSrc} alt="아바타" className="w-[6.75rem]" />
-            <h2 className="mt-10 font-bold font-montserrat text-2xl leading-9">{title}</h2>
+            <h2 className="mt-10 font-montserrat text-2xl font-bold leading-9">{title}</h2>
             <p className="mt-6 font-montserrat text-lg leading-[1.875rem] text-[#000000cc]">{description}</p>
-            <button className="mt-6 font-exo-2 font-bold text-lg leading-[1.875rem] text-[#18a0fb] hover:underline">
+            <button className="mt-6 font-exo-2 text-lg font-bold leading-[1.875rem] text-[#18a0fb] hover:underline">
                 LEARN MORE
             </button>
         </div>
--- src/components/profileSection/index.tsx
+++ src/components/profileSection/index.tsx
@@ -3,7 +3,7 @@ import Title from '@/components/Title';
 
 export default function ProfileSection() {
     return (
-        <section className="px-20 pt-[7.5rem] pb-[6.875rem]">
+        <section className="px-20 pb-[6.875rem] pt-[7.5rem]">
             <Title>
                 Snap photos and share like <br />
                 never before
