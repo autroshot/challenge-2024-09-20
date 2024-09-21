cee4083a19396dbf02d6e825119afb5751f2b61c
autroshot <autroshot@gmail.com>
2024-09-21 15:08:34 +0900

    불필요한 요소 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import ProfileSection from '@/components/profileSection';
 function App() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
             <ProfileSection />
         </>
     );
