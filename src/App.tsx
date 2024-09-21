9ef1a28083fef9086e094264eff9cbbb9195ecd3
autroshot <autroshot@gmail.com>
2024-09-21 15:16:28 +0900

    main 태그 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import ProfileSection from '@/components/profileSection';
 
 function App() {
     return (
-        <>
+        <main>
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
             <ProfileSection />
-        </>
+        </main>
     );
 }
 
