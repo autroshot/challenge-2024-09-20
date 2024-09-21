c5e945a60489e13cb8bfcdc0acd334e1891c614a
autroshot <autroshot@gmail.com>
2024-09-21 12:55:36 +0900

    ProfileSection 컴포넌트 구현 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import ProfileSection from '@/components/profileSection';
+
 function App() {
     return (
         <>
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <ProfileSection />
         </>
     );
 }
---
+++ src/components/profileSection/index.tsx
@@ -0,0 +1,13 @@
+import Title from '@/components/Title';
+
+export default function ProfileSection() {
+    return (
+        <section className="px-20 pt-[7.5rem] pb-[6.875rem]">
+            <Title>
+                Snap photos and share like <br />
+                never before
+            </Title>
+            <div className="pt-[4.375rem]">카드 목록</div>
+        </section>
+    );
+}
