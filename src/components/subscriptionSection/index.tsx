b91da40224afaaed43021dc0e3db6eb3966ae174
autroshot <autroshot@gmail.com>
2024-09-23 02:23:58 +0900

    리팩터: 변수 이름 변경 bgUrl -> defaultBackgroundUrl

--- src/components/subscriptionSection/index.tsx
+++ src/components/subscriptionSection/index.tsx
@@ -1,11 +1,15 @@
-import bgUrl from '@/assets/default-background.jpg';
+import defaultBackgroundUrl from '@/assets/default-background.jpg';
 import Description from '@/components/subscriptionSection/Description';
 import EmailForm from '@/components/subscriptionSection/EmailForm';
 
 export default function SubscriptionSection() {
     return (
         <section className="relative h-[740px]">
-            <img src={bgUrl} alt="" className="absolute left-0 top-0 -z-20 block h-full w-full object-cover" />
+            <img
+                src={defaultBackgroundUrl}
+                alt=""
+                className="absolute left-0 top-0 -z-20 block h-full w-full object-cover"
+            />
             <div className="absolute left-0 top-0 -z-10 h-full w-full bg-[#00000080]" />
             <div className="h-full w-full px-20 py-[9.5rem] text-center">
                 <Description />
