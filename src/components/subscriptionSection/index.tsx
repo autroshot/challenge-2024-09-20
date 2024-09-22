6500e7181efce861f5563ff5760a60881fea1d7c
autroshot <autroshot@gmail.com>
2024-09-23 02:49:59 +0900

    SubscriptionSection 컴포넌트에서 배경 이미지에 랜덤 가져오기 구현

--- src/components/subscriptionSection/index.tsx
+++ src/components/subscriptionSection/index.tsx
@@ -1,12 +1,39 @@
 import defaultBackgroundUrl from '@/assets/default-background.jpg';
 import Description from '@/components/subscriptionSection/Description';
 import EmailForm from '@/components/subscriptionSection/EmailForm';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function SubscriptionSection() {
+    const [backgroundImageUrl, setBackgroundImageUrl] = useState(defaultBackgroundUrl);
+
+    const LOCAL_STORAGE_KEY = 'background-image-url';
+    useEffect(() => {
+        const oldBackgroundImageUrl = localStorage.getItem(LOCAL_STORAGE_KEY);
+        if (oldBackgroundImageUrl !== null) {
+            setBackgroundImageUrl(oldBackgroundImageUrl);
+
+            return;
+        }
+
+        axios
+            .get('https://api.unsplash.com/photos/random', {
+                headers: { Authorization: `Client-ID ${import.meta.env.VITE_UNSPLASH_CLIENT_ID}` },
+            })
+            .then((value) => {
+                const newBackgroundImageUrl = value.data.urls.full;
+                localStorage.setItem(LOCAL_STORAGE_KEY, newBackgroundImageUrl);
+
+                setBackgroundImageUrl(newBackgroundImageUrl);
+
+                return;
+            });
+    }, []);
+
     return (
         <section className="relative h-[740px]">
             <img
-                src={defaultBackgroundUrl}
+                src={backgroundImageUrl}
                 alt=""
                 className="absolute left-0 top-0 -z-20 block h-full w-full object-cover"
             />
